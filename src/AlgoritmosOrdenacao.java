1bcd77e85f2afc6649d547586435056eea34a9a7
Filipe <fbianchi.andrade@gmail.com>
2024-02-27 12:42:56 -0300

    criando metodos estaticos de ordenação de lista. selection sort e inserction sort crescente e decrescente

--- src/AlgoritmosOrdenacao.java
+++ src/AlgoritmosOrdenacao.java
@@ -1,10 +1,10 @@
 public class AlgoritmosOrdenacao {
     public static void main(String[] args) {
-        // int[] lista = {64, 34, 25, 12, 22, 11, 90};
-        int [] lista = GeraListaDeInteiros.geraLista();
+        int[] lista = {64, 34, 25, 12, 22, 11, 90};
+        // int [] lista = GeraListaDeInteiros.geraLista();
         System.out.println("Lista antes da ordenação.");
-        // imprimirLista(lista);
-        selectionSort(lista);
+        imprimirLista(lista);
+        selectionSortCrescente(lista);
         System.out.println("\nLista depois da ordenação crescente.");
         imprimirLista(lista);
 
@@ -51,7 +51,7 @@ public class AlgoritmosOrdenacao {
         return lista;
     }
 
-    public static int[] selectionSort(int [] lista) {
+    public static int[] selectionSortCrescente (int [] lista) {
         int n = lista.length;
         // Loop externo que percorre a lista
         for (int i = 0; i < n - 1; i++) {
@@ -73,7 +73,54 @@ public class AlgoritmosOrdenacao {
 
         return lista;
     }
+    
+    public static int[] selectionSortDecrescente (int [] lista) {
+        int n = lista.length;
+        // Loop externo que percorre a lista
+        for (int i = 0; i < n - 1; i++) {
+            // Assumindo que o elemento atual é o menor
+            int minIndex = i;
+            // Loop interno que compara elemento atual com demais elementos
+            for (int j = i + 1; j < n; j++) {
+                // Se encontrarmos um elemento menor que o atual atualizamos o indice minimo (minIndex)
+                if(lista[j] > lista[minIndex])  {
+                    minIndex = j;
+                }
+            }
 
+            // Após encontrarmos o elemento menor, trocamos sua posição com o elemento atual
+            int temp = lista[minIndex];
+            lista[minIndex] = lista[i];
+            lista[i] = temp;
+        }
+
+        return lista;
+    }
+
+    public static int[] inserctionSortCrescente(int [] lista) {
+        int i, j, chave;
+        for (i = 1; i < lista.length; i++) {
+            chave = lista[i];
+            for (j = i - 1; (j >= 0) && (lista[j] > chave); j--) {
+                lista[j + 1] = lista[j];
+            }
+            lista[j+1] = chave;
+        }       
+        return lista;
+    }
+
+    public static int[] inserctionSortDecrescente(int [] lista) {
+        int i, j, chave;
+        for (i = 1; i < lista.length; i++) {
+            chave = lista[i];
+            for (j = i - 1; (j >= 0) && (lista[j] < chave); j--) {
+                lista[j + 1] = lista[j];
+            }
+            lista[j+1] = chave;
+        }       
+        return lista;
+    }
+  
     public static void imprimirLista(int [] lista) {
         for (int i = 0; i < lista.length; i++) {
             if (i > 0) {
--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         Fila fila = new Fila();
+        Scanner in = new Scanner(System.in);
 
         fila.inserir("Filipe");
         fila.inserir("Ianca");
@@ -8,10 +11,30 @@ public class Principal {
         fila.inserir("Olavo");
         fila.inserir("Olivia");
 
-        fila.listar();
-        
-        System.out.println("Elemento atendido: " + fila.atender());
-        System.out.println("Pessoas na fila: ");
-        fila.listar();
+        while (true) {
+            System.out.println("Escolha uma opção");
+            System.out.println("I - Inserir");
+            System.out.println("A - Atender");
+            System.out.println("L - listar fila");
+            System.out.println("Para sair digite qualquer Letra");
+            String op = in.nextLine();
+            op = op.toLowerCase();
+            
+            if (op.equals("i")) {
+                System.out.println("Digite o nome da pessoa: ");
+                String nome = in.nextLine();
+                fila.inserir(nome);
+                System.out.println("");
+            } else if(op.equals("a")) {
+                System.out.println("O Próximo elemento da fila é " + fila.atender());
+                System.out.println("");
+            } else if(op.equals("l")) {
+                fila.listar();
+                System.out.println("");
+            }  else {
+                in.close();
+                break;
+            }
+        }
     }
 }
